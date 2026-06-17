d38c72b1f44e762f44ec3827bc0affe7f3fc6f8a
Khadijah <83687477+ummairah@users.noreply.github.com>
2026-06-17 18:03:57 +0800

    Add files via upload

---
+++ library.h
@@ -0,0 +1,33 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int MAX = 100;
+
+struct Book {
+    string title;
+    string isbn;
+    string genre;
+    string publisher;
+};
+
+// Ainaa
+void addBook(Book library[], int& bookCount);
+void displayBooks(Book library[], int bookCount);
+
+// Search (Athirah & Dee)
+void searchMenu(Book library[], int bookCount);
+void searchByISBN(Book library[], int bookCount);
+void searchByTitle(Book library[], int bookCount);
+
+// Sort (Jemang & Haja)
+void sortMenu(Book library[], int bookCount);
+void sortByTitle(Book library[], int bookCount);
+void sortByGenre(Book library[], int bookCount);
+void sortByPublisher(Book library[], int bookCount);
+
+#endif
